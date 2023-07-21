b7338cf74446e9a6522f3eca5e5c38a85a324a21
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-07-21 18:49:07 +0100

    Word counter app completed

--- wordCounterApp/myApp/views.py
+++ wordCounterApp/myApp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def retrieve(request):
+    text_input = request.POST['textInput']
+    # spliting and counting the texts
+
+    number_of_words = len(text_input.split())
+
+    context = {
+        'number': number_of_words
+    }
+
+    return render(request, 'retrieve.html', context)
